8e23a0b326e229b477490971a2f83d884d52f923
Spenser Leighton <spenser.leighton@gmail.com>
2019-03-03 11:51:35 -0700

    Fix BG size

--- components/hero/views/main-hero.php
+++ components/hero/views/main-hero.php
@@ -6,12 +6,12 @@
 ?>
 
 
-
-<section id="hero">
-    <div class="hero-main" style="background-image: url('<?php echo get_stylesheet_directory_uri().'/assets/images/burns_wall.gif'; ?>');"></div>
-    <!-- <video class="hero-main" autoplay loop id="video-background" muted plays-inline>
+    <!-- <video class="hero-mai" autoplay loop id="video-background" muted plays-inline>
         <source src="<?php echo get_stylesheet_directory_uri().'/assets/images/burns.mp4'; ?>" type="video/mp4">
     </video> -->
+
+<section id="hero">
+    <div class="hero-main" style="background-image: url('<?php echo get_stylesheet_directory_uri().'/assets/images/burns-wall_.gif'; ?>');"></div>
     <div class="hero-content">
         <h1 class="hero-title" id="hero-title"><?php echo $DATA->hero_text; ?></h1>
     </div>
