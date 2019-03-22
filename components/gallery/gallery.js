1233dc710174b22e342394d571464a2a5488d8e3
Spenser Leighton <spenser.leighton@gmail.com>
2019-03-22 17:27:51 -0600

    New gallery

--- components/gallery/gallery.js
+++ components/gallery/gallery.js
@@ -14,13 +14,13 @@
 
    
 
-    // Get the modal
-    var modal = document.getElementById('myModal');
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() { 
-    modal.style.display = "none";
-    }
+    // // Get the modal
+    // var modal = document.getElementById('myModal');
+    // // Get the <span> element that closes the modal
+    // var span = document.getElementsByClassName("close")[0];
+    // // When the user clicks on <span> (x), close the modal
+    // span.onclick = function() { 
+    // modal.style.display = "none";
+    // }
 
 })(jQuery);
--- components/gallery/views/main-gallery.php
+++ components/gallery/views/main-gallery.php
@@ -6,35 +6,12 @@ if($DATA){ ?>
         <div class="row gallery-container">
             <?php foreach($DATA->images as $image){ ?>
 
-                <div class="gallery-box">
-                    <img 
-                    style="display: none" 
-                    src="<?php echo $image->url; ?>" 
-                    title="<?php echo $image->title; ?>" 
-                    description="<?php echo $image->description; ?>"
-                    year="<?php echo $image->year; ?>"
-                    >
-                    <div class="bg-image" style="background-image: url('<?php echo $image->url; ?>')"></div>
-                    <div class="gallery-inner">
-                        <h2 class="gallery-title">
-                            <?php echo $image->title; ?>
-                        </h2> 
+                <div class="gallery-image">
+                    <img src="<?php echo $image->url; ?>">
+                    <div>
+                        <h4><?php echo $image->title; ?><span><?php echo $image->description; ?></span></h4>
                     </div>
                 </div>
-
-                <!-- The Modal -->
-                <div id="myModal" class="modal">
-                    <span class="close">&times;</span>
-                    <img class="modal-content" src="" id="img01">
-                    <div class="gallery-details">
-                        <h2>
-                            <span id="title"></span>
-                            <span id="year"></span>
-                        </h2>
-                        <p id="description"></p>
-                    </div>
-                </div>
-
             <?php } ?>
         </div>
     </div>
